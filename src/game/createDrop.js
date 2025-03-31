365297baadd367d29b8358e48bbc5129115a93a6
Dirk Plate <email@dplate.de>
2025-03-31 18:02:30 +0200

    Avoid dropping through walls

--- src/game/createDrop.js
+++ src/game/createDrop.js
@@ -9,25 +9,17 @@ export default (position) => {
     element.style.transition = 'left 1s linear, top 1s linear';
 
     return {
-        lastPosition: { ...position },
         position,
-        nextPosition: { ...position },
-        force: {
-            x: 0,
-            y: 0
-        },
+        forces: [],
         speed: {
             x: 10,
             y: -5
         },
-        pressures: [],
-        maxPressure: 0,
         time: performance.now() / 1000.0,
-        airResistanceFactor: 2.2,
         coreRadius: 10.0,
         softRadius: 25.0,
         adhesiveness: 5.0,
-        elasticity: 25.0,
+        elasticity: 40.0,
         element
     };
 }
--- src/game/createPhysics.js
+++ src/game/createPhysics.js
@@ -1,4 +1,5 @@
 const meterInPixels = 100;
+const maxSpeed = 1.5;
 
 const updatePosition = (drop) => {
     drop.position.x = drop.element.offsetParent.scrollLeft + drop.element.offsetLeft
@@ -7,67 +8,14 @@ const updatePosition = (drop) => {
 
 const initializeDrop = (drop) => {
     updatePosition(drop);
-    drop.pressures = [];
-    drop.force.x = 0;
-    drop.force.y = 0;
+    drop.forces = [];
 };
 
 const addGravity = (drop) => {
-  drop.force.y += 9.81;
-};
-
-const addAirResistance = (drop) => {
-    drop.force.x -= drop.airResistanceFactor * drop.speed.x;
-    drop.force.y -= drop.airResistanceFactor * drop.speed.y;
-};
-
-const applyWallCollision = (drop, walls) => {
-    const movement = {
-        x: drop.position.x - drop.lastPosition.x,
-        y: drop.position.y - drop.lastPosition.y
-    };
-    const movementLength = Math.sqrt(movement.x**2 + movement.y**2);
-    if (movementLength === 0) {
-        return;
-    }
-    const coreRadiusFactor = drop.coreRadius / movementLength;
-
-    const firstCollision = walls.map(wall => {
-        const denominator = movement.x * wall.length.y - movement.y * wall.length.x;
-        if (denominator === 0) {
-            return null;
-        }
-        const lastPositionToWall = {
-            x: wall.position.x - drop.lastPosition.x,
-            y: wall.position.y - drop.lastPosition.y
-        };
-        const movementCollision = (lastPositionToWall.x * wall.length.y - lastPositionToWall.y * wall.length.x) / denominator;
-        if (movementCollision < 0.0 || movementCollision > (1.0 + coreRadiusFactor)) {
-            return null;
-        }
-        const wallCollision = (lastPositionToWall.x * movement.y - lastPositionToWall.y * movement.x) / denominator;
-        if (wallCollision < 0.0 || wallCollision > 1.0) {
-            return null;
-        }
-        return {
-            wall,
-            factor: movementCollision
-        };
-    }).filter(collision => collision !== null).sort((collision1, collision2) => collision1.factor - collision2.factor)[0];
-
-    if (firstCollision === undefined) {
-        return;
-    }
-    const { wall, factor } = firstCollision;
-
-    const dotProduct = drop.speed.x * wall.length.x + drop.speed.y * wall.length.y;
-    const uSquared = wall.length.x**2 + wall.length.y**2;
-    const scale = 2 * (dotProduct / uSquared);
-
-    drop.speed.x = (scale * wall.length.x - drop.speed.x) * 0.5;
-    drop.speed.y = (scale * wall.length.y - drop.speed.y) * 0.5;
-    drop.position.x = drop.lastPosition.x + (factor - coreRadiusFactor) * movement.x;
-    drop.position.y = drop.lastPosition.y + (factor - coreRadiusFactor) * movement.y;
+  drop.forces.push({
+      x: 0.0,
+      y: 9.81
+  });
 };
 
 const addPressure = (drop, pressurePosition, offset) => {
@@ -78,7 +26,7 @@ const addPressure = (drop, pressurePosition, offset) => {
     const distance = Math.sqrt(pressureVector.x**2 + pressureVector.y**2);
     const pressureNormalized = Math.min((offset - distance) / (drop.softRadius - drop.coreRadius), 1.0);
     if (distance > 0.0 && pressureNormalized > 0.0) {
-        drop.pressures.push({
+        drop.forces.push({
             x: (pressureVector.x / distance) * (drop.adhesiveness - pressureNormalized * drop.elasticity),
             y: (pressureVector.y / distance) * (drop.adhesiveness - pressureNormalized * drop.elasticity)
         })
@@ -113,25 +61,119 @@ const addDropPressures = (drop, drops) => {
     });
 };
 
-const applyPressures = (drop) => {
-    drop.pressures.forEach(pressure => {
-        drop.force.x += pressure.x;
-        drop.force.y += pressure.y;
-    });
+const findWallCollision = (position, vector, wall) => {
+    const denominator = vector.x * wall.length.y - vector.y * wall.length.x;
+    if (denominator === 0) {
+        return null;
+    }
+    const positionToWall = {
+        x: wall.position.x - position.x,
+        y: wall.position.y - position.y
+    };
+    const vectorCollisionFactor = (positionToWall.x * wall.length.y - positionToWall.y * wall.length.x) / denominator;
+    if (vectorCollisionFactor < 0.0 || vectorCollisionFactor > 1.0) {
+        return null;
+    }
+    const wallCollision = (positionToWall.x * vector.y - positionToWall.y * vector.x) / denominator;
+    if (wallCollision < 0.0 || wallCollision > 1.0) {
+        return null;
+    }
+    return vectorCollisionFactor;
 };
 
-const applyForce = (drop, elapsedTime) => {
-    drop.lastPosition.x = drop.position.x;
-    drop.lastPosition.y = drop.position.y;
+const findDropCollision = (position, vector, drop) => {
+    const ap = {
+        x: drop.position.x - position.x,
+        y: drop.position.y - position.y
+    };
+    const t = Math.min(1.0,
+        (ap.x * vector.x + ap.y * vector.y) / (vector.x**2 + vector.y**2)
+    );
+    if (t <= 0.0) {
+        return null;
+    }
+    const nearestPosition = {
+        x: position.x + t * vector.x,
+        y: position.y + t * vector.y
+    };
+    const distance = Math.sqrt(
+        (nearestPosition.x - drop.position.x)**2 + (nearestPosition.y - drop.position.y)**2
+    );
+    if (distance > drop.coreRadius) {
+        return null;
+    }
+    return t;
+};
 
-    drop.speed.x += drop.force.x * elapsedTime
-    drop.speed.y += drop.force.y * elapsedTime
+const avoidCollisions = (drop, nextPosition, walls, drops) => {
+    const movementVector = {
+        x: nextPosition.x - drop.position.x,
+        y: nextPosition.y - drop.position.y
+    };
+    const movementLength = Math.sqrt(movementVector.x**2 + movementVector.y**2);
+    if (movementLength === 0) {
+        return 1.0;
+    }
+    const coreRadiusFactor = drop.coreRadius / movementLength;
 
-    drop.nextPosition.x = drop.position.x + drop.speed.x * meterInPixels;
-    drop.nextPosition.y = drop.position.y + drop.speed.y * meterInPixels;
+    const collisionFactors = [];
+    walls.forEach(wall => {
+        const collisionFactor = findWallCollision(drop.position, movementVector, wall);
+        if (collisionFactor !== null) {
+            collisionFactors.push(collisionFactor - coreRadiusFactor);
+        }
+    });
+    drops.forEach(otherDrop => {
+        if (drop !== otherDrop) {
+            const collisionFactor = findDropCollision(drop.position, movementVector, otherDrop);
+            if (collisionFactor !== null) {
+                collisionFactors.push(collisionFactor);
+            }
+        }
+    });
+    if (collisionFactors.length === 0) {
+        return 1.0;
+    }
+    const nearestCollisionFactors = collisionFactors
+        .sort((collision1, collision2) => collision1 - collision2);
+    const nearestCollisionFactor = nearestCollisionFactors[0];
 
-    drop.element.style.left = `${drop.nextPosition.x}px`;
-    drop.element.style.top = `${drop.nextPosition.y}px`;
+    nextPosition.x = drop.position.x + movementVector.x * nearestCollisionFactor;
+    nextPosition.y = drop.position.y + movementVector.y * nearestCollisionFactor;
+
+    return nearestCollisionFactor;
+};
+
+const applyForces = (drop, elapsedTime, walls, drops) => {
+    const force = {
+        x: 0.0,
+        y: 0.0
+    }
+    drop.forces.forEach(pressure => {
+        force.x += pressure.x;
+        force.y += pressure.y;
+    });
+
+    drop.speed.x += force.x * elapsedTime;
+    drop.speed.y += force.y * elapsedTime;
+
+    const speed = Math.sqrt(drop.speed.x**2 + drop.speed.y**2);
+    if (speed !== 0) {
+        const reducedSpeed = Math.min(speed, maxSpeed);
+        drop.speed.x *= reducedSpeed / speed;
+        drop.speed.y *= reducedSpeed / speed;
+    }
+
+    const nextPosition = {
+        x: drop.position.x + drop.speed.x * meterInPixels,
+        y: drop.position.y + drop.speed.y * meterInPixels
+    };
+    const movementReductionFactor = avoidCollisions(drop, nextPosition, walls, drops);
+
+    const animationSlowness = Math.max(0.1, movementReductionFactor);
+    drop.element.style.left = `${nextPosition.x}px`;
+    drop.element.style.top = `${nextPosition.y}px`;
+    drop.element.style.transition = `left ${animationSlowness}s linear, top ${animationSlowness}s linear`;
 };
 
 export default (level) => {
@@ -142,26 +184,12 @@ export default (level) => {
         const elapsedTime = currentTime - drop.time;
 
         initializeDrop(drop);
-
         addGravity(drop);
-        addAirResistance(drop);
-
-        applyWallCollision(drop, level.walls);
-
         addWallPressures(drop, level.walls);
         addDropPressures(drop, drops);
-
-        applyPressures(drop);
-
-        applyForce(drop, elapsedTime);
+        applyForces(drop, elapsedTime, level.walls, drops);
 
         drop.time = currentTime;
-
-        /*
-        console.log('ElapsedTime', elapsedTime);
-        console.log('MaxPressure', drop.maxPressure);
-        console.log('Force', drop.force);
-        console.log('Speed', drop.speed);*/
 
         window.setTimeout(() => moveDrop(drop), 50);
     };
